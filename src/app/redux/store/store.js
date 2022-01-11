5ac198ffa322c130e1459f2cf0b5921c589c577d
Max <gmaxgomes@gmail.com>
2022-01-11 13:06:46 -0300

    new createAction

--- src/app/redux/store/store.js
+++ src/app/redux/store/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { dummyReducer } from "../dummyReducer";
 
 export const store = configureStore({
-  reducer: {dummy: dummyReducer}
+  reducer: {}
 })
